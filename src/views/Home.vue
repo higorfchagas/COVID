0e9e15efbeacf40e4a09d08975d68e71d363d18b
Higor Chagas <42949902+higorfchagas@users.noreply.github.com>
2020-08-20 10:16:52 -0300

    Esboço e get de paises, diretamente da api.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="content">
+      <div class="container">
+       <div class="row">
+         <div class="col-md-2">
+
+         </div>
+         <div class="col-md-8">
+                 País:
+              <multiselect v-model="SelecaoPaises" :options="Paises" placeholder="Selecione um país"></multiselect>
+         </div>
+         <div class="col-md-2">
+           
+         </div>
+       </div>
+      </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import Multiselect from 'vue-multiselect'
+  import { http } from '@/services/config'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    Multiselect 
+  },
+   data () {
+      return {
+        SelecaoPaises: '',
+         Paises:[],
+      }
+},
+methods: {
+    getPais: function () {
+      var self = this;
+      const axios = require('axios');
+      axios.get('https://api.covid19api.com/countries')
+          .then(function (response) {
+          self.Paises = response.data;
+          })
+          .catch(function () {
+          console.log('Não foi possível carregar os países, contate ao administrador do site.');
+          })
+    },
+    listarPaises:() => {
+      var self = this;
+        return http.get('countries')
+        .then(function (response) {
+          self.Paises = response.data;
+          })
+          .catch(function () {
+          alert('Não foi possível carregar os países, contate ao administrador do site.');
+          })
+    },
+  },
+mounted(){
+this.getPais();
+},
 }
 </script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
