e519fe32d7aeecbeeb7f12a90448e526eddc0cc8
Higor Chagas <42949902+higorfchagas@users.noreply.github.com>
2020-08-23 01:13:51 -0300

    verificação status api e aviso caso esteja off

--- src/services/paises.js
+++ src/services/paises.js
@@ -1,7 +1,17 @@
 import { http } from './config'
+import axios from 'axios'
 
 export default {
     listarPaises:() => {
         return http.get('countries')
     },
+    verificarStatusApi:() => {
+        const httpStatusApi = axios.create({
+            baseURL: 'https://covid19-brazil-api.now.sh/api/'
+        });
+        return httpStatusApi.get('status/v1')
+    },
+
+
+    
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,17 +5,25 @@
   >
     <div class="content">
       <div class="container">
+        <div class="mt-3" v-if="!StatusApi">
+          <b-card-group deck>
+            <b-card bg-variant="danger" text-variant="white" header="Status" class="text-center">
+              <b-card-text>A fonte de dados foi inativada, contate ao administrador do site.</b-card-text>
+            </b-card>
+          </b-card-group>
+        </div>
+        <br />
+        <div v-if="StatusApi"></div>
         <div class="card textCard">
           <h3 class="card-header">COVID ANALYZER</h3>
           <div class="card-body">
-            <p class="card-text">Evolução da doença sars-covid19 por países.</p>
             <p>COVID-19 (do inglês Coronavirus Disease 2019) é uma doença infeciosa causada pelo coronavírus da síndrome respiratória aguda grave 2 (SARS-CoV-2). Os sintomas mais comuns são febre, tosse seca e cansaço. Entre outros sintomas menos comuns estão dores musculares, dor de garganta, dor de cabeça, congestão nasal, conjuntivite, perda do olfato e do paladar e erupções cutâneas. Cerca de 80% dos casos confirmados são ligeiros ou assintomáticos e a maioria recupera sem sequelas. No entanto, 15% são infeções graves que necessitam de oxigénio e 5% são infeções muito graves que necessitam de ventilação assistida em ambiente hospitalar. Os casos mais graves podem evoluir para pneumonia grave com insuficiência respiratória grave, septicémia, falência de vários órgãos e morte. Entre os sinais de agravamento da doença estão a falta de ar, dor ou pressão no peito, dedos de tom azul ou perturbações na fala e no movimento. O agravamento pode ser súbito, ocorre geralmente durante a segunda semana e requer atenção médica urgente.</p>
           </div>
         </div>
         <br />
         <div class="row">
-          <div class="col-md-2"></div>
-          <div class="col-md-8">
+          <div class="col-md-3"></div>
+          <div class="col-md-6">
             País:
             <multiselect
               v-model="PaisSelecionado"
@@ -24,38 +32,39 @@
               track-by="country"
               placeholder="Selecione um país"
               :searchable="true"
+              :disabled="StatusApi == false && !Paises"
             ></multiselect>
           </div>
-          <div class="col-md-2"></div>
+          <div class="col-md-3"></div>
         </div>
         <br />
         <br />
         <div class="row textCard" v-if="PaisSelecionado != ''">
           <div class="col-md-3">
             <b-card-group deck>
-              <b-card bg-variant="light" header="País" class="text-center">
-                <b-card-text>{{ PaisSelecionado.country }}</b-card-text>
+              <b-card bg-variant="light" header="País" class="textHeader text-center">
+                <b-card-text class="textCard">{{ PaisSelecionado.country }}</b-card-text>
               </b-card>
             </b-card-group>
           </div>
           <div class="col-md-3">
             <b-card-group deck>
-              <b-card bg-variant="light" header="Confirmados" class="text-center">
-                <b-card-text>{{ PaisSelecionado.confirmed }}</b-card-text>
+              <b-card bg-variant="light" header="Confirmados" class="textHeader text-center">
+                <b-card-text class="textCard">{{ PaisSelecionado.confirmed }}</b-card-text>
               </b-card>
             </b-card-group>
           </div>
           <div class="col-md-3">
             <b-card-group deck>
-              <b-card bg-variant="light" header="Recuperados" class="text-center">
-                <b-card-text>{{ PaisSelecionado.recovered }}</b-card-text>
+              <b-card bg-variant="light" header="Recuperados" class="textHeader text-center">
+                <b-card-text class="textCard">{{ PaisSelecionado.recovered }}</b-card-text>
               </b-card>
             </b-card-group>
           </div>
           <div class="col-md-3">
             <b-card-group deck>
-              <b-card bg-variant="light" header="Mortes" class="text-center">
-                <b-card-text>{{ PaisSelecionado.deaths }}</b-card-text>
+              <b-card bg-variant="light" header="Mortes" class="textHeader text-center">
+                <b-card-text class="textCard">{{ PaisSelecionado.deaths }}</b-card-text>
               </b-card>
             </b-card-group>
           </div>
@@ -78,8 +87,8 @@ import Multiselect from "vue-multiselect";
 import ServicesPaises from "@/services/paises";
 import Ads from "vue-google-adsense";
 import Vue from "vue";
-import { BCard } from "bootstrap-vue";
-Vue.component("b-card", BCard);
+import { CardPlugin } from 'bootstrap-vue'
+Vue.use(CardPlugin)
 Vue.use(require("vue-script2"));
 Vue.use(Ads.Adsense);
 
@@ -92,6 +101,7 @@ export default {
     return {
       PaisSelecionado: [],
       Paises: [],
+      StatusApi: false,
     };
   },
   methods: {
@@ -107,9 +117,26 @@ export default {
           );
         });
     },
+    checkApi: function () {
+      var self = this;
+      ServicesPaises.verificarStatusApi()
+        .then(function (response) {
+          if (response.data.status == "ok") {
+            self.StatusApi = true;
+            self.getPais();
+          } else {
+            self.StatusApi = false;
+          }
+        })
+        .catch(function () {
+          alert(
+            "Não foi possível carregar os países, contate ao administrador do site."
+          );
+        });
+    },
   },
   mounted() {
-    this.getPais();
+    this.checkApi();
   },
 };
 </script>
@@ -136,5 +163,10 @@ export default {
 
 .textCard {
   color: black;
+  font-weight: normal;
+}
+
+.textHeader {
+  font-weight: bold;
 }
 </style>
